8fd8f052e1e15263bee914a06cb6ba21b653173b
MichalJar <michal.jarmakowicz@gmail.com>
2018-02-04 13:24:49 +0100

    tests, evolution

--- include/clustree.hpp
+++ include/clustree.hpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <functional>
 #include <algorithm>
+#include <tuple>
 
 namespace stream{
 
@@ -8,25 +9,25 @@ template<typename CF>
 class clustree
 {
     using update_cf = std::function<void (CF & cluster_feature, const CF & new_element)>;
-    using distance_between_cf  = std::function<void (const CF & cf_a, const CF & cf_b)>;
+    using distance_between_cf  = std::function<double (const CF & cf_a, const CF & cf_b)>;
 
     class node
     {
     public:
-        std::vector<cf> cluster_features;
+        std::vector<CF> cluster_features;
         std::vector<std::shared_ptr<node>> childs;
-    }
+    };
 
     using node_ptr = std::shared_ptr<node>;
 
 public:
-    clustree(int node_max_width, int cf_max_diameter, update_cf update, distance_between_cf distance):
+    clustree(unsigned int node_max_width, double cf_max_diameter, update_cf update, distance_between_cf distance):
         node_max_width(node_max_width), 
         cf_max_diameter(cf_max_diameter),
         update(update),
         distance(distance)
     {
-        root = std::make_shared<Node>();
+        root = std::make_shared<node>();
     }
 
     void add_element(const CF & cf_created_from_element)
@@ -34,39 +35,87 @@ public:
         if(root->cluster_features.size() == 0)
         {
             root->cluster_features.push_back( cf_created_from_element );
-            root->childs.push_back(nullptr)
+            root->childs.push_back(nullptr);
         }
         else
         {
             add_element_to_no_empty_tree(cf_created_from_element);
         }
     }
+
+    node_ptr root;
 private:
     void add_element_to_no_empty_tree(const CF & cf_created_from_element)
     {
-        auto n_ptr = traverse_and_update(element);
+        auto n_ptr = traverse_and_update(cf_created_from_element);
 
         const int i = get_nearest_cf_index_from(cf_created_from_element, n_ptr);
         if(distance(cf_created_from_element, n_ptr->cluster_features[i]) <= cf_max_diameter)
         {
-            update_cf(n_ptr->cluster_features[i], cf_created_from_element);
+            update(n_ptr->cluster_features[i], cf_created_from_element);
         }
-        else if( n_ptr->cluster_features.size() >= node_max_width)
-        {
-            update_cf(n_ptr->cluster_features[i], cf_created_from_element);
-            n_ptr->childs[i] = create_new_leaf(cf_created_from_element);
-        }
-        else 
+        else if( n_ptr->cluster_features.size() < node_max_width)
         {
             n_ptr->cluster_features.push_back(cf_created_from_element);
             n_ptr->childs.push_back(nullptr);
         }
+        else 
+        {
+            n_ptr->childs[i] = create_new_leaf(cf_created_from_element, n_ptr->cluster_features[i]);
+            update(n_ptr->cluster_features[i], cf_created_from_element);
+        }
     }
 
-    node_ptr create_new_leaf(const & cf_created_from_element)
+    void split(node_ptr & n_ptr, const CF & cf_created_from_element)
     {
-        node_ptr new_n_ptr = std::make_shared<Node>();
+        auto & cfs = n_ptr->cluster_features;
+        cfs.push_back(cf_created_from_element);
+
+        unsigned int cf_i=0, cf_y=0;
+
+        std::tie(cf_i, cf_y) = get_nearest_cfs_in(cfs);
+
+        node_ptr new_n_ptr = std::make_shared<node>();
+        new_n_ptr->cluster_features.push_back(cfs[cf_i], cfs[cf_y]);
+        new_n_ptr->childs.push_back(nullptr);
+        new_n_ptr->childs.push_back(nullptr);
+
+        n_ptr->childs = new_n_ptr;
+        update(cfs[cf_i],cfs[cf_y]);
+        cfs.erase(std::begin(cfs) + cf_y);
+    }
+
+    auto get_nearest_cfs_in(const std::vector<CF> & cfs)
+    {
+        unsigned int cf_i = 0;
+        unsigned int cf_y = 1;
+
+        double d = distance(cfs[0],cfs[1]);
+
+        for(int i=0;i<cfs.size()-1;i++)
+        {
+            for(int y=i+1;y<cfs.size();y++)
+            {
+                auto nd = distance(cfs[i],cfs[y]);
+                if(d > nd)
+                {
+                    d = nd;
+                    cf_i = i;
+                    cf_y = y;
+                }
+            }
+        }
+        return std::make_tuple(cf_i, cf_y);
+    }
+
+    node_ptr create_new_leaf(const CF & cf_created_from_element, const CF & parent_cf)
+    {
+        node_ptr new_n_ptr = std::make_shared<node>();
+
+        new_n_ptr->cluster_features.push_back(parent_cf);
         new_n_ptr->cluster_features.push_back(cf_created_from_element);
+        
+        new_n_ptr->childs.push_back(nullptr);
         new_n_ptr->childs.push_back(nullptr);
         return new_n_ptr;
     }
@@ -83,36 +132,31 @@ private:
             }
             else
             {
-                update_cf(n_ptr->cluster_features[i], cf_created_from_element);
+                update(n_ptr->cluster_features[i], cf_created_from_element);
                 n_ptr = n_ptr->childs[i];
             }
         }
         return n_ptr;
     }
 
-    const int get_nearest_cf_index_from(const cf<e> & new_cf, const node_ptr n_ptr)
+    const int get_nearest_cf_index_from(const CF & new_cf, const node_ptr n_ptr)
     {
         auto const & cfs = n_ptr->cluster_features;
         int best_index = 0;
-        int d = dist(new_cf, cfs[0]);
+        int d = distance(new_cf, cfs[0]);
 
         for(auto i=1; i< cfs.size(); i++)
         {
-            if(d > dist(new_cf, cfs[i]))
+            if(d > distance(new_cf, cfs[i]))
             {
-                d = dist(new_cf, cfs[i]);
+                d = distance(new_cf, cfs[i]);
                 best_index = i;
             }
         }
         return best_index;
     }
-
-private:
-    const int node_max_width;
+    const unsigned int node_max_width;
     const double cf_max_diameter;
-
-    node_ptr root;
-
     update_cf update;
     distance_between_cf distance;
 };
---
+++ tests/clustreetest.cpp
@@ -0,0 +1,132 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <clustree.hpp>
+#include <cmath>
+
+struct cluster_feature
+{
+    double ls[2];
+    double ss[2];
+    int num;
+};
+
+void update_cf(cluster_feature & old_cf, const cluster_feature & new_cf)
+{
+    old_cf.ls[0] += new_cf.ls[0];
+    old_cf.ls[1] += new_cf.ls[1];
+    old_cf.ss[0] += new_cf.ss[0];
+    old_cf.ss[1] += new_cf.ss[1];
+    old_cf.num += new_cf.num;
+}
+
+double distance_between_cf(const cluster_feature & a, const cluster_feature & b)
+{
+    auto d0 = a.ls[0]/a.num - b.ls[0] / b.num;
+    auto d1 = a.ls[1]/a.num - b.ls[1] / b.num;
+    return std::sqrt(d0*d0 + d1*d1);
+}
+
+TEST_CASE("clustree creation", "[clustree]")
+{
+    stream::clustree<cluster_feature> ct(2,3.0,update_cf, distance_between_cf);
+}
+
+TEST_CASE("add element to clustree", "[clustree]")
+{
+    stream::clustree<cluster_feature> ct(2,3.0,update_cf, distance_between_cf);
+    cluster_feature ncf = {{2.0,1.0},{4.0,1.0},1};
+
+    ct.add_element(ncf);
+
+    REQUIRE( ct.root->cluster_features[0].ls[0] == 2.0 );
+    REQUIRE( ct.root->cluster_features[0].ls[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[0] == 4.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[0].num == 1 );
+}
+
+TEST_CASE("add two elements to clustree -> merge", "[clustree]")
+{
+    stream::clustree<cluster_feature> ct(2,3.0,update_cf, distance_between_cf);
+
+    cluster_feature ncf1 = {{2.0,1.0},{4.0,1.0},1};
+    cluster_feature ncf2 = {{4.0,2.0},{16.0,1.0},1};
+
+    ct.add_element(ncf1);
+    ct.add_element(ncf2);
+
+    REQUIRE( ct.root->cluster_features[0].ls[0] == 6.0 );
+    REQUIRE( ct.root->cluster_features[0].ls[1] == 3.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[0] == 20.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[1] == 2.0 );
+    REQUIRE( ct.root->cluster_features[0].num == 2 );
+}
+
+TEST_CASE("add two elements to clustree -> two cfs in root", "[clustree]")
+{
+    stream::clustree<cluster_feature> ct(2,3.0,update_cf, distance_between_cf);
+
+    cluster_feature ncf1 = {{2.0,1.0},{4.0,1.0},1};
+    cluster_feature ncf2 = {{4.0,4.0},{16.0,1.0},1};
+
+    ct.add_element(ncf1);
+    ct.add_element(ncf2);
+
+    REQUIRE( ct.root->cluster_features.size() == 2 );
+
+    REQUIRE( ct.root->cluster_features[0].ls[0] == 2.0 );
+    REQUIRE( ct.root->cluster_features[0].ls[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[0] == 4.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[0].num == 1 );
+
+    REQUIRE( ct.root->cluster_features[1].ls[0] == 4.0 );
+    REQUIRE( ct.root->cluster_features[1].ls[1] == 4.0 );
+    REQUIRE( ct.root->cluster_features[1].ss[0] == 16.0 );
+    REQUIRE( ct.root->cluster_features[1].ss[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[1].num == 1 );
+}
+
+TEST_CASE("add three elements to clustree -> two cfs in root and third in new leaf","[clustree]")
+{
+    stream::clustree<cluster_feature> ct(2,3.0,update_cf, distance_between_cf);
+
+    cluster_feature ncf1 = {{2.0,1.0},{4.0,1.0},1};
+    cluster_feature ncf2 = {{4.0,4.0},{16.0,1.0},1};
+    cluster_feature ncf3 = {{0.0,-2.0},{0.0,4.0},1};
+
+    ct.add_element(ncf1);
+    ct.add_element(ncf2);
+    ct.add_element(ncf3);
+
+    REQUIRE( ct.root->cluster_features.size() == 2 );
+    REQUIRE( ct.root->childs.size() == 2 );
+    REQUIRE( ct.root->childs[0] != nullptr );
+    REQUIRE( ct.root->childs[1] == nullptr );
+
+    REQUIRE( ct.root->cluster_features[0].ls[0] == 2.0 );
+    REQUIRE( ct.root->cluster_features[0].ls[1] == -1.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[0] == 4.0 );
+    REQUIRE( ct.root->cluster_features[0].ss[1] == 5.0 );
+    REQUIRE( ct.root->cluster_features[0].num == 2 );
+
+    REQUIRE( ct.root->cluster_features[1].ls[0] == 4.0 );
+    REQUIRE( ct.root->cluster_features[1].ls[1] == 4.0 );
+    REQUIRE( ct.root->cluster_features[1].ss[0] == 16.0 );
+    REQUIRE( ct.root->cluster_features[1].ss[1] == 1.0 );
+    REQUIRE( ct.root->cluster_features[1].num == 1 );
+
+    REQUIRE( ct.root->childs[0]->cluster_features.size() == 2 );
+
+    REQUIRE( ct.root->childs[0]->cluster_features[0].ls[0] == 2.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[0].ls[1] == 1.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[0].ss[0] == 4.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[0].ss[1] == 1.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[0].num == 1 );
+
+    REQUIRE( ct.root->childs[0]->cluster_features[1].ls[0] == 0.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[1].ls[1] == -2.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[1].ss[0] == 0.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[1].ss[1] == 4.0 );
+    REQUIRE( ct.root->childs[0]->cluster_features[1].num == 1 );
+}
